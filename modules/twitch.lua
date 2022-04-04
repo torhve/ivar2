92a4a7023ebb464a253fabdd7b259279aafe2743
Tor Hveem <tor@hveem.no>
2022-04-04 16:13:54 +0200

    twitch: use new API

--- modules/twitch.lua
+++ modules/twitch.lua
@@ -7,23 +7,56 @@ local moduleName = 'twitch'
 local key = moduleName
 local store = ivar2.persist
 
+local oauth_bearer = ''
+
+local function getToken()
+	local tokenurl = "https://id.twitch.tv/oauth2/token?client_id="
+		..ivar2.config.twitchApiKey
+		..'&client_secret='
+		..ivar2.config.twitchApiSecret
+		..'&redirect_uri=http://localhost&response_type=token&grant_type=client_credentials&scope='
+
+	simplehttp({
+		url = tokenurl,
+		method = 'POST',
+			headers = {
+				--['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8',
+				['Authorization'] = string.format( "OAuth %s", ivar2.config.twitchApiSecret)
+			},
+		},
+		function(data)
+			local info = json.decode(data)
+			-- Save access token for further use
+			oauth_bearer = info.access_token
+		end
+	)
+	return true
+	end
+
 local twitchAPICall = function(url, cb)
+	-- get initial token
+	if oauth_bearer == '' then
+		getToken()
+	end
+
 	return simplehttp({
 		url='https://api.twitch.tv' .. url,
 		-- HTTP doesn't allow lowercase headers
 		version=1.1,
 		headers={
+			['Authorization'] = string.format("Bearer %s", oauth_bearer),
 			['Client-ID'] = ivar2.config.twitchApiKey,
 			['Accept'] = 'application/vnd.twitchtv.v5+json',
 		}
-	})
+	}, cb)
 end
 
-local gameLookup = function(id)
-		local data = twitchAPICall(string.format('/helix/games?id=%s', id))
+local streamLookup = function(user_login)
+		local data = twitchAPICall(string.format('/helix/streams?user_login=%s', user_login))
+
 		data = json.decode(data)
-		local name = data['data'][1]['name']
-		return name
+		local stream = data['data'][1]
+		return stream
 end
 
 local formatViewers = function(viewers)
@@ -44,15 +77,15 @@ local parseData = function(self, source, destination, data, search)
 
 	for i=1, #data.data do
 		local this = data.data[i]
-		local lang = this.language
+		local lang = this.broadcaster_language
 		--TODO configure filter languages ?
 		if lang and (lang == 'en' or lang == 'no') then
 			if search then
 				-- Check for search string in game or channel name
 				-- since twitch search API also does title search
-				if this.channel.display_name:lower():match(search:lower())
-					or (this.game ~= json.null
-					and this.game:lower():match(search:lower())) then
+				if this.display_name:lower():match(search:lower())
+					or (this.game_name ~= json.null
+					and this.game_name:lower():match(search:lower())) then
 					table.insert(streams, this)
 				end
 			else
@@ -61,8 +94,6 @@ local parseData = function(self, source, destination, data, search)
 		end
 	end
 
-	-- sort streams wrt viewer count
-	table.sort(streams, function(a,b) return a.viewer_count>b.viewer_count end)
 	return streams
 end
 
@@ -71,11 +102,17 @@ local formatData = function(self, source, destination, streams, limit)
 	local i = 0
 	local out = {}
 	for _, stream in pairs(streams) do
-		local viewers = formatViewers(stream.viewer_count)
+		local stream_data = streamLookup(stream.broadcaster_login)
+		stream.viewer_count = stream_data.viewer_count
+	end
+	-- sort streams wrt viewer count
+	table.sort(out, function(a,b) return a.viewer_count or 0 >b.viewer_count or 0 end)
+	for _, stream in pairs(streams) do
 		local title = ': '..stream.title
+		local viewers = formatViewers(stream.viewer_count or 0)
 		out[#out+1] = string.format(
 			"[%s] http://twitch.tv/%s %s %s",
-			util.bold(viewers), stream.user_name, gameLookup(stream.game_id), title
+			util.bold(viewers), stream.broadcaster_login, stream.game_name, title
 		)
 		i=i+1
 		if i > limit then break end
@@ -85,8 +122,11 @@ end
 
 local gameHandler = function(self, source, destination, input, limit)
 	limit = limit or 5
-	local data = twitchAPICall('/helix/streams?limit='..tostring(limit)..'&offset=0&user_login='..util.urlEncode(input))
-	local streams = parseData(self, source, destination, data)
+	local data = twitchAPICall('/helix/search/channels?first='
+		..tostring(limit)
+		..'&live_only=true&query='
+		..util.urlEncode(input))
+	local streams = parseData(self, source, destination, data, input)
 	if #streams == 0 then
 		local out = 'No streams found'
 		self:Msg('privmsg', destination, source, out)
@@ -123,37 +163,31 @@ local checkStreams = function()
 		local alerts = store[alertsKey] or {}
 		for name, game in pairs(games) do
 			local limit = 5
-			simplehttp({
-				url=string.format(
-					'https://api.twitch.tv/kraken/search/streams?limit=%s&offset=0&query=%s',
-					tostring(limit),
-					util.urlEncode(game.name)
-				),
-				headers={
-					['Client-ID'] = ivar2.config.twitchApiKey,
-				}},
-				function(data)
-					local streams = parseData(ivar2, nil, c, data, game.name)
-					for _, stream in pairs(streams) do
-						-- Use Created At to check for uniqueness
-						if alerts[stream.channel.name] ~= stream.created_at and
-							-- Check if we meet viewer limit
-							stream.viewers > game.limit then
-							alerts[stream.channel.name] = stream.created_at
-							store[alertsKey] = alerts
-							ivar2:Msg('privmsg',
-								game.channel,
-								ivar2.nick,
-								"[%s] [%s] %s %s",
-								util.bold(game.name),
-								util.bold(formatViewers(stream.viewers)),
-								'http://twitch.tv/'..stream.channel.display_name,
-								stream.channel.status
-							)
-						end
-					end
+			local data = twitchAPICall('/helix/search/channels?first='
+				..tostring(limit)
+				..'&live_only=true&query='
+				..util.urlEncode(game.name))
+			local streams = parseData(ivar2, nil, c, data, game.name)
+			for _, stream in pairs(streams) do
+				-- Use started_at to check for uniqueness
+				local stream_data = streamLookup(stream.broadcaster_login)
+				stream.viewers = stream_data.viewer_count
+				if alerts[stream.broadcaster_login] ~= stream.started_at and
+					-- Check if we meet viewer limit
+					stream.viewers > game.limit then
+					alerts[stream.broadcaster_login] = stream.started_at
+					store[alertsKey] = alerts
+					ivar2:Msg('privmsg',
+						game.channel,
+						ivar2.nick,
+						"[%s] [%s] %s %s",
+						util.bold(game.name),
+						util.bold(formatViewers(stream.viewers)),
+						'https://twitch.tv/'..stream.display_name,
+						stream.title
+					)
 				end
-			)
+			end
 		end
 	end
 end
